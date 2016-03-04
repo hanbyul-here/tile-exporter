bbd9242959599cb01132b36dc5a695c883f3264f
hanbyul-here <hanbyul@nyu.edu>
2016-03-04 11:02:32 -0500

    removing uncs dependancy

--- app/Components/Search/ResultRow.js
+++ app/Components/Search/ResultRow.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router';
 
 export default class ResultRow extends Component{
 
   render() {
     const { data, dataIndex, rowIndex, pointAction } = this.props
     return(
-      <li className={(dataIndex === rowIndex)? "select table-view-cell search-result" : "table-view-cell search-result"} onClick= {() => pointAction(data)} > {data.properties.label} </li> 
+      <li className={(dataIndex === rowIndex)? "select table-view-cell search-result" : "table-view-cell search-result"} onClick= {() => pointAction(data)} > {data.properties.label} </li>
     );
   }
 }
