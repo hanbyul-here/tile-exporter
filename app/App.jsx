ed5ee1112a892f902635da4ebf29cb2779d69139
Hanbyul Jo <hanbyul@nyu.edu>
2016-11-07 19:13:25 -0500

    edited comment, little ordering

--- app/App.jsx
+++ app/App.jsx
@@ -3,18 +3,19 @@ import { render } from 'react-dom';
 
 import Keys from './Keys';
 import SearchBox from './Components/Search/SearchBox';
+
 import TileExporter from './Components/TileExporter/Exporter';
 
 require('./scss/main.scss');
+
+// Tile Exporter is not written as React Component
+const tileExporter = new TileExporter();
 
 const searchConfig = {
   placeholder: 'Search address or or place',
   childClass: 'searchBox',
   key: Keys.search
 };
-
-// Tile Exporter is not written as Reac Component
-const tileExporter = new TileExporter();
 
 function SearchBoxWrapper() {
   return (
