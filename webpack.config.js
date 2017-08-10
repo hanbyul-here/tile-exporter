63967d2df4bcbf378f9143c95a99114240bc32cb
Hanbyul Jo <hanbyul@nyu.edu>
2017-08-10 15:11:36 -0400

    duplicated deploy

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ const config = {
     './app/App.jsx'
   ],
   output: {
-    path: path.resolve(__dirname, './dist'),
+    path: path.resolve(__dirname, './'),
     filename: 'bundle.js'
   },
   plugins: [
